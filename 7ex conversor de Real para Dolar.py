6beaf5591e5d41a0fa2af6e24fa0fedcb36a0d87
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-18 22:58:34 -0300

    Add files via upload

---
+++ 7ex conversor de Real para Dolar.py
@@ -0,0 +1,13 @@
+
+
+
+#7. Faça um programa que dado um valor em reais (R$), mostre quantos dólares ($) a pessoa
+#poderia comprar
+
+print(f"\nValor do Dolar Atual USD:5,21\n")
+
+Reais = float(input("Quantos Reais voce tem ? R$:"))
+
+op = (Reais/5.21)
+
+print(f"\nVoce pode Comprar USD:{op:,.2f}\n")
